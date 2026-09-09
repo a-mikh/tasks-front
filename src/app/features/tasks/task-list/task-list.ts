1a4d7c25a97dc556a58eef7cad8b1d8b6a653f7b
Anton <amikhalchenkonton@gmail.com>
2026-09-09 17:04:48 +0200

    feat: add task list states

--- src/app/features/tasks/task-list/task-list.ts
+++ src/app/features/tasks/task-list/task-list.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TaskApiService } from '../../../services/task-api-service';
 import { Task } from '../../../models/task';
 import { RouterLink } from '@angular/router';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
@@ -13,14 +14,27 @@ export class TaskList implements OnInit {
   private readonly taskApiService = inject(TaskApiService);
 
   protected readonly tasks = signal<Task[]>([]);
+  protected readonly isLoading = signal(false);
+  protected readonly errorMessage = signal<string | null>(null);
 
   ngOnInit(): void {
     this.loadTasks();
   }
 
-  private loadTasks(): void {
-    this.taskApiService.getTasks().subscribe((response) => {
-      this.tasks.set(response.content);
-    });
+  protected loadTasks(): void {
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+
+    this.taskApiService
+      .getTasks()
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (response) => {
+          this.tasks.set(response.content);
+        },
+        error: () => {
+          this.errorMessage.set('Failed to load tasks.');
+        },
+      });
   }
 }
