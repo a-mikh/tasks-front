b21f6e04d5330370970efee7265fbf5983d56fe9
Anton <amikhalchenkonton@gmail.com>
2026-09-09 20:32:28 +0200

    feat: add task details states

--- src/app/features/tasks/task-details/task-details.ts
+++ src/app/features/tasks/task-details/task-details.ts
@@ -2,6 +2,8 @@ import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TaskApiService } from '../../../services/task-api-service';
 import { Task } from '../../../models/task';
+import { finalize } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-task-details',
@@ -17,25 +19,47 @@ export class TaskDetails implements OnInit {
     const task = this.task();
     return task !== null && task.status !== 'DONE';
   });
+  protected readonly isLoading = signal(false);
+  protected readonly errorMessage = signal<string | null>(null);
 
   ngOnInit(): void {
     this.loadTaskDetails();
   }
 
-  private loadTaskDetails(): void {
+  protected loadTaskDetails(): void {
     const taskId = Number(this.activatedRoute.snapshot.paramMap.get('taskId'));
-    this.tasksService.getTaskById(taskId).subscribe((task) => {
-      this.task.set(task);
-    });
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+
+    this.tasksService
+      .getTaskById(taskId)
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (task) => {
+          this.task.set(task);
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            this.errorMessage.set('Task not found.');
+          } else {
+            this.errorMessage.set('Failed to load task.');
+          }
+        },
+      });
   }
 
   protected moveToNextStatus(taskId: number): void {
     if (!this.canAdvanceStatus()) {
       return;
     }
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
 
-    this.tasksService.moveToNextStatus(taskId).subscribe((task) => {
-      this.task.set(task);
-    });
+    this.tasksService
+      .moveToNextStatus(taskId)
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe((task) => {
+        this.task.set(task);
+      });
   }
 }
